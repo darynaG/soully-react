a7098de172864367389807cca76e08a582314e7f
Admin <admin@cavefish.edvantis.com>
2019-06-12 11:20:16 +0300

    hjghjufv

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import '././css/header_s.css';
+import '././css/footer.css';
 import Swiper from './components/swiper/swiper';
 import Header from './components/header/header';
+import Footer from './components/footer/footer'
 
 function App() {
   return (
     <div className="App">
    
       <Header/>
+      <Footer/>
     </div>
   );
 }

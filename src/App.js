fb2fb3dba54043bcf75cda815946a9acf817dbed
Admin <admin@cavefish.edvantis.com>
2019-06-25 14:56:28 +0300

    demo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
-/*
 import '../src/css/header-responsible.css'
-*/
 import Header from './components/header/header';
 import MoodPage  from './components/mood-group/moodPage';
 import Footer from '../src/components/footer/footer';
@@ -11,23 +9,21 @@ import Day from '../src/components/Route/Day'
 import Month from '../src/components/Route/Month'
 import Guest from '../src/components/Route/Guest'
 import StoryPage from '../src/components/Route/StoryPage'
-import HamburgerIconContainer from '../src/containers/HamburgerIconContainer'
+import Burger from '../src/components/header/BurgerIcon'
+
 
 class App extends React.Component {
   render(){
   return (
     <Router>
       <div>
-      
         <Header/>
-        <HamburgerIconContainer showSideBarMenu />
         <Route exact path="/" component={MoodPage} />
         <Route exact path="/day" component={Day} />
         <Route exact path="/guest" component={Guest} />
         <Route exact path="/stories" component={StoryPage} />
         <Route exact path="/month" component={Month} />
-        
-        
+        <Burger/>
         <Footer/>
       </div>
     </Router>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,11 @@
 import { combineReducers } from "redux";
 import currentday from "./currentDay";
 import changeMood from './changeMood';
-import burgerMenu from './burgerMenuReducer';
 
 const rootReducer = combineReducers({
   currentday,
-  changeMood,
-  burgerMenu
+  changeMood
+
 });
 
 export default rootReducer;

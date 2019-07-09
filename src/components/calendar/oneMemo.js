c124859fed9b31f726d61a55918a8a66b1f47f93
daryna hatsosh <daryna.hatsosh@edvantis.com>
2019-07-09 15:01:07 +0300

    f

--- src/components/calendar/oneMemo.js
+++ src/components/calendar/oneMemo.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import "../../css/oneMemo.css";
+import "../../css/OneMemo.css";
 
 class OneMemo extends Component {
   state = {
